b2640fa7482c7f4eb54613cd8cba2177a804f6a7
liuyuanfeng <liuyuanfeng1@stu.gdou.edu.cn>
2022-08-12 20:40:28 +0800

    不带头指针的单链表

--- chapter3/linkedList.c
+++ chapter3/linkedList.c
@@ -1,15 +1,65 @@
-# include <stdio.h>
-# include <stdlib.h>
-#define ERROR -1
-typedef enum bool{false,true} bool;
+#include <stdio.h>
+#include <stdlib.h>
+
+#define ERROR NULL
 typedef int ElementType;
-typedef struct LNode* PtrToLNode;
-struct LNode{
+typedef struct LNode *PtrToLNode;
+struct LNode {
     ElementType Data;
     PtrToLNode Next;
 };
 typedef PtrToLNode Position;
 typedef PtrToLNode List;
+
+Position Find( List L, ElementType X );
+List Insert( List L, ElementType X, Position P );
+List Delete( List L, Position P );
+
+int main()
+{
+    List L;
+    ElementType X;
+    Position P, tmp;
+    int N;
+
+    L = NULL;
+    scanf("%d", &N);
+    while ( N-- ) {
+        scanf("%d", &X);
+        L = Insert(L, X, L);
+        if ( L==ERROR ) printf("Wrong Answer\n");
+    }
+    scanf("%d", &N);
+    while ( N-- ) {
+        scanf("%d", &X);
+        P = Find(L, X);
+        if ( P == ERROR )
+            printf("Finding Error: %d is not in.\n", X);
+        else {
+            L = Delete(L, P);
+            printf("%d is found and deleted.\n", X);
+            if ( L==ERROR )
+                printf("Wrong Answer or Empty List.\n");
+        }
+    }
+    L = Insert(L, X, NULL);
+    if ( L==ERROR ) printf("Wrong Answer\n");
+    else
+        printf("%d is inserted as the last element.\n", X);
+    P = (Position)malloc(sizeof(struct LNode));
+    tmp = Insert(L, X, P);
+    if ( tmp!=ERROR ) printf("Wrong Answer\n");
+    tmp = Delete(L, P);
+    if ( tmp!=ERROR ) printf("Wrong Answer\n");
+    for ( P=L; P; P = P->Next ) printf("%d ", P->Data);
+    return 0;
+}
+
+/* 你的代码将被嵌在这里 */
+// 不带头结点的单链表
+Position Find( List L, ElementType X );
+List Insert( List L, ElementType X, Position P );
+List Delete( List L, Position P );
 
 int Length(List L){
     Position p;
@@ -22,20 +72,6 @@ int Length(List L){
     return cnt;
 }
 
-ElementType FindKth(List L,int K){
-    Position p=L;
-    int cnt=1; // 位序从1开始
-    while(p&&cnt<K){
-        p=p->Next;
-        cnt++;
-    }
-    if(cnt==K&&p){
-        return p->Data;
-    }else{
-        return ERROR;
-    }
-}
-
 Position Find(List L,ElementType X){
     Position p=L;
     while(p&&p->Data!=X){
@@ -44,35 +80,33 @@ Position Find(List L,ElementType X){
     return p;
 }
 
-Position _FindPre(List L,int index){
+Position _FindPre(List L,Position P){
     Position p=L;
-    int cnt=0;
-    while(p&&cnt<index-1){
+    while(p&&p->Next!=P){
         p=p->Next;
-        cnt++;
     }
-    if(p==NULL || cnt!=index-1){
-        return NULL;
+    if(p==NULL){
+        return ERROR;
     }else{
         return p;
     }
 } 
 
-List Insert(List L,ElementType X,int i){
+List Insert(List L,ElementType X,Position P){
     Position tmp,pre;
-    tmp = (Position) malloc(sizeof(struct LNode));
+    tmp =(List) malloc(sizeof(struct LNode));
     tmp ->Data =X;
     tmp ->Next =NULL;
 
-    if(i==1){ // 头插入(位序)
-        tmp ->Next=L;
+    if(L==P){ // 头插入(位序)
+        tmp->Next=L;
         return tmp;
     }else{
-        pre=_FindPre(L,i);
+        pre=_FindPre(L,P);
         if(pre==NULL){
-            printf("插入错误\n");
+            printf("Wrong Position for Insertion\n");
             free(tmp);
-            return NULL;
+            return ERROR;
         }else{
             tmp->Next=pre->Next;
             pre->Next=tmp;
@@ -81,19 +115,19 @@ List Insert(List L,ElementType X,int i){
     }
 }
 
-List Delete( List L, int i ){
+List Delete( List L, Position P ){
     Position p,tmp;
-    if(i==1){
+    if(L==P){
         p=L;
         tmp = p;
         p=p->Next;
         free(tmp);
         return p;
     }else{
-        p=_FindPre(L,i);
-        if(p==NULL){
-            printf("删除错误\n");
-            return L;
+        p=_FindPre(L,P);
+        if(p==ERROR){
+            printf("Wrong Position for Deletion\n");
+            return ERROR;
         }else{
             tmp = p->Next;
             p->Next=tmp->Next;
